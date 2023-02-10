bd4e0340f3e142b1ba8b13705e4fe2cabcb5502b
Daniel Abu <99013608+dan-abu@users.noreply.github.com>
2023-02-10 08:51:07 +0000

    Create Exercise 23 - File Overlap.py

---
+++ Exercise 23 - File Overlap.py
@@ -0,0 +1,34 @@
+import time
+start_time = time.time()
+
+with open('/content/drive/MyDrive/Colab Notebooks/primenumbers.txt', 'r') as open_file:
+    all_text = open_file.read()
+
+p_list = all_text.split('\n')
+
+with open('/content/drive/MyDrive/Colab Notebooks/happynumbers.txt', 'r') as open_file:
+    all_text = open_file.read()
+
+h_list = all_text.split('\n')
+
+primelist = []
+for x in p_list:
+  x = int(x)
+  primelist.append(x)
+
+happylist = []
+for x in h_list:
+  x = int(x)
+  happylist.append(x)
+
+print(primelist)
+print(happylist)
+
+mutual_list = []
+for x in primelist:
+  if x in happylist:
+    mutual_list.append(x)
+
+print(mutual_list)
+
+print("--- %s seconds ---" % (time.time() - start_time))
